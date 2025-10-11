e334c9c1868dbde21608ad94065487b079285d92
unknown <zunaira2k@gmail.com>
2025-10-11 16:33:07 +0530

    Changes for matrix

--- 11102025/program4.py
+++ 11102025/program4.py
@@ -1,11 +1,13 @@
-print("ADD TWO MATRIX")
-print("FIRST MATRIX:")
-r1 = int(input("Please enter number of rows"))
-c1 = int(input("Please enter number of columns"))
 
-matrix1 = []
+n=int(input("Enter the number of Rows of matrices:- "))         # Taking the dimensions from the user
+m=int(input("Enter the number of Columns of matrices:- "))
 
-print("SECOND MATRIX:")
+print("Enter the first Matrix")
+A=[[int(input(f"A[{i}][{j}]: "))for j in range(m)] for i in range(n)]   # using list comprehension method for entering the matrices.
+                                                                                                    # Format= ( Expresion for item in iterable)
 
-r2 = int(input("Please enter number of rows"))
-c2 = int(input("Please enter number of columns"))
+print("Enter the second Matrix")
+B=[[int(input(f"B[{i}][{j}]: "))for j in range(m)] for i in range(n)]
+
+Addition= [[A[i][j] + B[i][j] for j in range(m)] for i in range(n)]       # Adding the matrices element using index and loops
+print("Addition Matrix of Matrix A and Matrix B is ", Addition)
